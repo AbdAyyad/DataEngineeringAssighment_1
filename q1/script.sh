f8bf4b644988a3ac2e17248d59fc63ac8025d55e
AbdAyyad <abdayyad2012@gmail.com>
2021-05-15 01:19:49 +0300

    question 2 README.MD and enhancements

--- q1/script.sh
+++ q1/script.sh
@@ -1,8 +1,7 @@
-docker stop "$(docker ps -q)" || true
-#docker rm "$(docker ps -aq)"
+docker stop "$(docker ps -q)" > /dev/null 2>&1
 
-docker network create nifi-network || true
-docker volume create --name=nifi_data || true
+docker network create nifi-network > /dev/null 2>&1
+docker volume create --name=nifi_data > /dev/null 2>&1
 
 docker-compose up -d
 
@@ -10,6 +9,6 @@ sleep 5
 
 docker cp ./file.sql de_postgres:/file.sql
 docker cp ./small.csv de_postgres:/file.csv
-docker exec -u postgres de_postgres psql postgres postgres -f /file.sql
+docker exec -u postgres de_postgres psql postgres postgres -f /file.sql > /dev/null 2>&1
 
 echo 'nifi is running on port 8080'
--- q2/script.sh
+++ q2/script.sh
@@ -1,3 +1,6 @@
+docker stop "$(docker ps -q)" > /dev/null 2>&1
+
+mkdir -p logs plugins
 docker-compose up airflow-init
 
 docker-compose up -d
@@ -6,6 +9,9 @@ sleep 5
 
 docker cp ./script.sql de_postgres:/file.sql
 docker cp ./small.csv de_postgres:/file.csv
-docker exec -u postgres de_postgres psql postgres postgres -f /file.sql
+docker exec -u postgres de_postgres psql postgres postgres -f /file.sql > /dev/null 2>&1
 docker exec airflow mkdir -p /home/airflow/data
+
+curl -X PATCH "http://localhost:8080/api/v1/dags/data_transformation" -H  "accept: application/json" -H  "Content-Type: application/json" -H "Authorization: Basic YWlyZmxvdzphaXJmbG93" -d "{\"is_paused\":false}"
+
 echo "airflow started on port 8080"
