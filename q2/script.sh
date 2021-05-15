efce195b3b7aae330d2f43812239c77a78454c29
AbdAyyad <abdayyad2012@gmail.com>
2021-05-15 17:29:10 +0300

    finalize_project

--- q2/script.sh
+++ q2/script.sh
@@ -1,7 +1,7 @@
 docker stop "$(docker ps -q)" > /dev/null 2>&1
 
-mkdir -p logs plugins
-docker-compose up airflow-init
+mkdir -p logs plugins postgres
+docker-compose up -d airflow-init
 
 docker-compose up -d
 
@@ -12,6 +12,8 @@ docker cp ./small.csv de_postgres:/file.csv
 docker exec -u postgres de_postgres psql postgres postgres -f /file.sql > /dev/null 2>&1
 docker exec airflow mkdir -p /home/airflow/data
 
-curl -X PATCH "http://localhost:8080/api/v1/dags/data_transformation" -H  "accept: application/json" -H  "Content-Type: application/json" -H "Authorization: Basic YWlyZmxvdzphaXJmbG93" -d "{\"is_paused\":false}"
+sleep 10
+
+curl -X PATCH "http://localhost:8080/api/v1/dags/data_transformation" -H  "accept: application/json" -H  "Content-Type: application/json" -H "Authorization: Basic YWlyZmxvdzphaXJmbG93" -d "{\"is_paused\":false}" > /dev/null 2>&1
 
 echo "airflow started on port 8080"
